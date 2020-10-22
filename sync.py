e590e1abab28688e8a172baaf5293ae4acbb3940
Austin Hendrix <namniart@gmail.com>
2020-10-22 09:30:24 -0700

    Connecting to piwigo

--- sync.py
+++ sync.py
@@ -4,12 +4,20 @@ import argparse
 import sys
 
 import osxphotos
+import piwigo
 
 def main():
+    print("Connecting to Piwigo server")
+    piwigo_url = "http://arg:8000"
+
+    piwigo_site = piwigo.Piwigo(piwigo_url)
+    print("Piwigo server version: {}".format(piwigo_site.pwg.getVersion()))
+
+    print("Loading iPhoto library")
     skip_folders = { "iPhoto Events" }
     photosdb = osxphotos.PhotosDB()
 
-    print(photosdb.album_info[0])
+    print("Checking albums")
     for album in photosdb.album_info:
         if len(album.folder_names) > 0 and album.folder_names[0] in skip_folders:
             continue
